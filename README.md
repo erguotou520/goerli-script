656dc8ae6fb0d349669ed9940936bc17e1c6862d
erguotou <erguotou525@gmail.com>
2020-06-15 16:25:51 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Goerli网络刷币
+谨慎使用！
+
+## 环境变量
+- `WALLET_ADDRESS` 钱包地址，必填
+- `MAX_TIMES` 最大刷币次数，默认10
---
+++ index.js
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer');
+
+// 请求一次
+async function requestOnce(page) {
+  await page.type('#receiver', process.env.WALLET_ADDRESS, 60);
+  // 谷歌人机校验
+  const frame = await page.frames().find(f => f.getAttribute('role') === 'presentation');
+  if (frame) {
+    const checkbox = await frame.$('.recaptcha-checkbox');
+    await checkbox.click({ delay: 22 });
+    // 等待被选中
+    await frame.waitForSelector('.recaptcha-checkbox-checked', { timeout: 3000 })
+    await page.waitFor(Math.random() * 2000)
+    // 点击request按钮
+    await page.click('#requestTokens', { delay: 50 })
+    await page.waitForSelector('.swal2-container.swal2-shown', { timeout: 3000 })
+    // 点击ok
+    await page.click('.swal2-confirm', { delay: 32 })
+    await page.waitFor(Math.random() * 3239);
+  }
+}
+
+(async () => {
+  const { WALLET_ADDRESS, MAX_TIMES = 10 } = process.env;
+  if (!WALLET_ADDRESS) throw new Error('Environment variables \'WALLET_ADDRESS\' must be set.')
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://goerli-faucet.slock.it/');
+  for (let i = 0; i < MAX_TIMES; i++) {
+    await requestOnce(page)
+  }
+  // 关闭
+  await browser.close();
+})();
