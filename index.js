eb97cb66166dbe67bd3fd4fc28233a2c73e9701b
erguotou <erguotou525@gmail.com>
2020-06-18 19:56:28 +0800

    一顿修改

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
-const puppeteer = require('puppeteer');
+// 参考自 https://github.com/danielgatis/puppeteer-recaptcha-solver/blob/master/index.js
+// const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra')
+const pluginStealth = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(pluginStealth())
+
+function rdn (min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min)) + min
+}
 
 // 请求一次
 async function requestOnce(page) {
-  await page.type('#receiver', process.env.WALLET_ADDRESS, 60);
-  // 谷歌人机校验
+  // 等待recaptcha加载完成
+  // await page.waitForFunction(() => {
+  //   const iframe = document.querySelector('iframe[src*="api2/anchor"]')
+  //   if (!iframe) return false
+  //   return !!iframe.contentWindow.document.querySelector('#recaptcha-anchor')
+  // })
+  await page.waitFor('iframe[src*="api2/anchor"]')
+  // 获取主Frame
   const frames = await page.frames()
-  const frame = frames.find(f => f.name() === 'Goerli Testnet Faucet');
+  // let mainFrame
+  // for (const _frame of frames) {
+  //   const title = await _frame.title()
+  //   if (title === 'Goerli Testnet Faucet') {
+  //     mainFrame = _frame
+  //     break
+  //   }
+  // }
+  // 子frame
+  // let frame = mainFrame.childFrames().find(_frame => _frame.url().match(/anchor.html$/))
+  const frame = frames.find(frame => frame.url().includes('api2/anchor'))
+  // 谷歌人机校验
   if (frame) {
-    const checkbox = await frame.$('.recaptcha-checkbox');
-    await checkbox.click({ delay: 22 });
+    // 输入钱包地址
+    await page.click('#receiver', { clickCount: 3 })
+    await page.type('#receiver', process.env.WALLET_ADDRESS, 60);
+    
+    // await frame.waitForSelector('.recaptcha-checkbox')
+    // await page.waitFor(Math.random() * 1200)
+    const checkbox = await frame.$('#recaptcha-anchor');
+    await checkbox.click({ delay: rdn(30, 150) });
+
     // 等待被选中
     await frame.waitForSelector('.recaptcha-checkbox-checked', { timeout: 3000 })
     await page.waitFor(Math.random() * 2000)
@@ -25,9 +59,20 @@ async function requestOnce(page) {
   try {
     const { WALLET_ADDRESS, MAX_TIMES = 10 } = process.env;
     if (!WALLET_ADDRESS) throw new Error('Environment variables \'WALLET_ADDRESS\' must be set.')
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
+    // https://github.com/puppeteer/puppeteer/issues/4053#issuecomment-567621325
+    const browser = await puppeteer.launch({
+      headless: false,
+      args:[
+        '--disable-web-security',
+        '--disable-features=IsolateOrigins,site-per-process'
+      ]
+    });
+    const [page] = await browser.pages();
+    page.setDefaultTimeout(0);
+
+    // await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
     await page.goto('https://goerli-faucet.slock.it/');
+    
     for (let i = 0; i < MAX_TIMES; i++) {
       await requestOnce(page)
     }
